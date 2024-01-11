188ec18811bc7a88f20e3d625d04b212626093c0
aizealawin <ktmudalige@gmail.com>
2024-01-11 16:46:01 -0500

    got first question to render

---
+++ app/assets/SurveyData.js
@@ -0,0 +1,37 @@
+export default data = [
+  {
+    question: "What type of hvac system do you have?",
+    answers: [
+      "Gas",
+      "Electric"
+    ]
+  },
+  {
+    question: "Let's break down your hvac type a little more:",
+    answers: [
+      "Central",
+      "Wall Unit"
+    ]
+  },
+  {
+    question: "How many hvac units are in your home?",
+    answers: null
+  },
+  {
+    question: "Your last gas bill was..?",
+    answers: null
+  },
+  {
+    question: "Your last electric bill was..?",
+    answers: null
+  },
+  {
+    question: "Your last water bill was..?",
+    answers: null
+  },
+  {
+    question: "Please enter your zip code",
+    answers: null
+  },
+  
+]
--- app/pages/Landing.js
+++ app/pages/Landing.js
@@ -1,11 +1,27 @@
 import { View, Text } from 'react-native'
 import { Link } from 'expo-router'
-import React from 'react'
+import React, { useState } from 'react'
 import Style from '../Style'
 import EcoFeatLogo from '../components/EcoFeatLogo'
+import data from "../assets/SurveyData"
 
 
 const Landing = () => {
+
+  const questions = [data[0]]
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleOptionClick = (answer) => {
+    const nextQuestion = currentQuestion + 1; 
+    if (nextQuestion < questions.length){
+      setCurrentQuestion(nextQuestion)
+    } else {
+      alert(`Test`)
+    }
+  }
+
+  console.log(questions[0].question)
 
 
   return (
@@ -13,11 +29,12 @@ const Landing = () => {
     
     <View style={Style.container}>
       <View style={Style.main}>
-        <Text>Image</Text>
+        <Text>{questions[currentQuestion].question}</Text>
+        {/* <Text>Image</Text>
         <Text>First, we are going to ask you a few simple questions about your living situation...</Text>
         <Link href=''>
           <Text>-&gt;</Text>
-        </Link>
+        </Link> */}
       </View>
     </View>
   )
